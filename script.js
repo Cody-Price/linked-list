7a772336d60a416c8e1baa51a88399593b39365e
Cody-Price <ctprice11@gmail.com>
2018-08-24 18:51:16 -0600

    Add working script to include read and delete buttons, said buttons do not work

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ var buttonAdd = document.querySelector('.button-add');
 
 create = buttonAdd.addEventListener('click', function () {
   var newElementLi = document.createElement('li');
+  var readButton = document.createElement('button', 'class="read-button"');
+  var deleteButton = document.createElement('button', 'class="delete-button"');
   var urlContent = document.querySelector('#url-input').value;
   var titleContent = document.querySelector('#title-input').value;
+  
   var titleUrlContent = titleContent + ' ' + urlContent;
+
   var newContent = document.createTextNode(titleUrlContent);
   newElementLi.appendChild(newContent);
   var userList = document.querySelector('#user-list');
 
   document.body.insertBefore(newElementLi, userList);
+  document.body.insertBefore(readButton, userList);
+  document.body.insertBefore(deleteButton, userList);
 
+  var newReadContent = document.createTextNode('Read');
+  var newDeleteContent = document.createTextNode('Delete');
+
+  readButton.appendChild(newReadContent);
+  deleteButton.appendChild(newDeleteContent);
 
   console.log('click');
 })
