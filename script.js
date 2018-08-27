1e28120f2616c58676e3a7e4b930cdfaeb626ee3
Cody-Price <ctprice11@gmail.com>
2018-08-26 19:38:03 -0600

    Add ability to give each read/delete button unique Id

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 var buttonAdd = document.querySelector('.button-add');
 
-create = buttonAdd.addEventListener('click', function () {
+var readQueryAll = function() {
+  return document.querySelectorAll('.read-button');
+}
+var deleteQueryAll = function() {
+  return document.querySelectorAll('.delete-button');
+}
+
+function addBookmark() {
   var newElementLi = document.createElement('li');
-  var readButton = document.createElement('button', 'class="read-button"');
-  var deleteButton = document.createElement('button', 'class="delete-button"');
+  var readButton = document.createElement('button');
+  var deleteButton = document.createElement('button');
   var urlContent = document.querySelector('#url-input').value;
   var titleContent = document.querySelector('#title-input').value;
+  var userList = document.querySelector('#user-list');
   
   var titleUrlContent = titleContent + ' ' + urlContent;
+  var newBookmark = document.createTextNode(titleUrlContent);
+  newElementLi.appendChild(newBookmark);
+ 
 
-  var newContent = document.createTextNode(titleUrlContent);
-  newElementLi.appendChild(newContent);
-  var userList = document.querySelector('#user-list');
+  userList.appendChild(newElementLi);
+  userList.appendChild(readButton);
+  userList.appendChild(deleteButton);
 
-  document.body.insertBefore(newElementLi, userList);
-  document.body.insertBefore(readButton, userList);
-  document.body.insertBefore(deleteButton, userList);
+  readButton.setAttribute('class', 'read-button');
+  deleteButton.setAttribute('class', 'delete-button');
+
 
   var newReadContent = document.createTextNode('Read');
   var newDeleteContent = document.createTextNode('Delete');
@@ -23,5 +34,51 @@ create = buttonAdd.addEventListener('click', function () {
   readButton.appendChild(newReadContent);
   deleteButton.appendChild(newDeleteContent);
 
-  console.log('click');
+  let readArray = Array.from(readQueryAll());
+  let deleteArray = Array.from(deleteQueryAll())
+
+  for (var i = 0; i < readArray.length; i++) {
+    readButton.setAttribute('id', [i]);
+  }
+  for (var i = 0; i < deleteArray.length; i++) {
+    deleteButton.setAttribute('id', [i] + 100);
+  }
+}
+
+createBookmark = buttonAdd.addEventListener('click', function () {
+  addBookmark();
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function readStyle() {
+//   var readUserButton = document.querySelector('.read-button');
+//   var deleteUserButton = document.querySelector('.delete-button');
+//   readUserButton.addEventListener('click', function() {
+//     document.querySelector('li').style.color = 'purple';
+//     console.log('click');
+//   })
+// }
+
+// document.querySelector('body').addEventListener('click', function () {
+//   readStyle();
+// })
+
+
