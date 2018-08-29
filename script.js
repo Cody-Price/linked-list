91f8a7fdb327e1312a096ce48a44fdc2b3c5496a
Cody-Price <ctprice11@gmail.com>
2018-08-28 18:20:58 -0600

    finish styling mobile app

--- script.js
+++ script.js
@@ -5,24 +5,25 @@ var urlBar =  document.querySelector('#url-input');
 var titleBar = document.querySelector('#title-input');
 var urlContent = document.querySelector('#url-input').value;
 var titleContent = document.querySelector('#title-input').value;
-var bookmarkTotal = document.querySelector('#link-total').innerText;
-bookmarkTotal = parseInt(bookmarkTotal);
+// var bookmarkTotal = document.querySelector('#link-total').innerText;
+// // bookmarkTotal = parseInt(bookmarkTotal);
 var total = 0;
 var totalRead = 0;
 
-var liQueryAll = function() {
-  return document.querySelectorAll('li');
-}
-var aQueryAll = function() {
-  return document.querySelectorAll('a');
-}
-var readQueryAll = function() {
-  return document.querySelectorAll('.read-button');
-}
-var deleteQueryAll = function() {
-  return document.querySelectorAll('.delete-button');
-}
+// var liQueryAll = function() {
+//   return document.querySelectorAll('li');
+// }
+// var aQueryAll = function() {
+//   return document.querySelectorAll('a');
+// }
+// var readQueryAll = function() {
+//   return document.querySelectorAll('.read-button');
+// }
+// var deleteQueryAll = function() {
+//   return document.querySelectorAll('.delete-button');
+// 
 
+//refactor this into a single append with .innerhtml
 function addBookmark() {
   var newElementDiv = document.createElement('div');
   var newElementLi = document.createElement('li');
@@ -31,9 +32,9 @@ function addBookmark() {
   var urlContent = document.querySelector('#url-input').value;
   var titleContent = document.querySelector('#title-input').value;
   var spanElement = document.createElement('span');
-
   var a = document.createElement('a');
   a.href = urlContent;
+
   a.innerHTML = urlContent;
 
   var newBookmark = document.createTextNode(titleContent);
@@ -75,7 +76,7 @@ function addBookmark() {
       a.setAttribute('class', 'read');
       readButton.setAttribute('class', 'read-button');
       totalRead++;
-      console.log(totalRead);
+      // console.log(totalRead);
       document.querySelector('#read-total').innerHTML = totalRead;
     }
   })
@@ -96,23 +97,23 @@ function addBookmark() {
  
   })
 
-  let liArray = Array.from(liQueryAll());
-  let readArray = Array.from(readQueryAll());
-  let deleteArray = Array.from(deleteQueryAll());
-  let aArray = Array.from(aQueryAll());
+  // let liArray = Array.from(liQueryAll());
+  // let readArray = Array.from(readQueryAll());
+  // let deleteArray = Array.from(deleteQueryAll());
+  // let aArray = Array.from(aQueryAll());
 
-  for (var i = 0; i < liArray.length; i++) {
-    newElementLi.setAttribute('id', [i]);
-  }
-  for (var i = 0; i < aArray.length; i++) {
-    a.setAttribute('id', [i] + 100)
-  }
-  for (var i = 0; i < readArray.length; i++) {
-    readButton.setAttribute('id', [i] + 1000);
-  }
-  for (var i = 0; i < deleteArray.length; i++) {
-    deleteButton.setAttribute('id', [i] + 10000);
-  }
+  // for (var i = 0; i < liArray.length; i++) {
+  //   newElementLi.setAttribute('id', [i]);
+  // }
+  // for (var i = 0; i < aArray.length; i++) {
+  //   a.setAttribute('id', [i] + 100)
+  // }
+  // for (var i = 0; i < readArray.length; i++) {
+  //   readButton.setAttribute('id', [i] + 1000);
+  // }
+  // for (var i = 0; i < deleteArray.length; i++) {
+  //   deleteButton.setAttribute('id', [i] + 10000);
+  // }
 
   total++;
   document.querySelector('#link-total').innerHTML = total;
